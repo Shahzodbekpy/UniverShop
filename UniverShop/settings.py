7b22a48c3da66414df8a16d7f79ed2981ac0ee9f
Shahzodbekpy <Shahzodbekpy@gamil.com>
2023-05-05 15:50:24 +0500

    deploy-settings

--- UniverShop/settings.py
+++ UniverShop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p%*7&_83l^h-$(s&95!4g9bwj&k%=ly_3p9(@0&j23hgp($v!u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,10 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    BASE_DIR / 'static'
-]
+import os
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
